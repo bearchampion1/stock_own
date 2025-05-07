bd0bcd8e31bef748a0d974cb1013454076f24d0c
bear0118 <dppss0118@gmail.com>
2025-05-07 14:22:53 +0800

    faetuere: 跟目錄GET

--- api/index.py
+++ api/index.py
@@ -36,6 +36,11 @@ handler = WebhookHandler(line_channel_secret)
 user_states = {}
 def  webhook_handler(environ, start_response):
     return app(environ, start_response)
+
+@app.route("/", methods=["GET"])
+def index():
+    return "The server is running!"
+
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
